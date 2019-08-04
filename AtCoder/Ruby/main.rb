551abb5296241922887871c31b56afe98635a36e
ikngtty <ikngtty@gmail.com>
2019-08-04 22:55:15 +0900

    improve templates to show how to get x, y, z

--- AtCoder/Ruby/main.rb
+++ AtCoder/Ruby/main.rb
@@ -4,11 +4,12 @@ module Enumerable
   end
 end
 
-n = gets.chomp.to_i
+n = gets.to_i
+x, y, z = gets.split(' ').map(&:to_i)
 a = Array.new(n)
 b = Array.new(n)
-(0...n).map { |i| a[i], b[i] = gets.chomp.split(' ').map(&:to_i) }
+(0...n).map { |i| a[i], b[i] = gets.split(' ').map(&:to_i) }
 
-total = a.zip(b).map(&:sum).sum
+total = a.zip(b).map(&:sum).sum + x + y + z
 
 puts total
