f9b6941abeaf6b97271cd1933b66c1635cc0b466
ikngtty <ikngtty@gmail.com>
2019-12-22 17:25:59 +0900

    add `chomp`
    It may be unnecessary but desirable to avoid bad smell.

--- AOJ/Ruby/main.rb
+++ AOJ/Ruby/main.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-n = gets.to_i
-x, y, z = gets.split(' ').map(&:to_i)
+n = gets.chomp.to_i
+x, y, z = gets.chomp.split(' ').map(&:to_i)
 a = Array.new(n)
 b = Array.new(n)
-(0...n).map { |i| a[i], b[i] = gets.split(' ').map(&:to_i) }
+(0...n).map { |i| a[i], b[i] = gets.chomp.split(' ').map(&:to_i) }
 
 total = a.zip(b).map(&:sum).sum + x + y + z
 
