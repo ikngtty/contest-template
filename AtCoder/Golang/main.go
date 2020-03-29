1fb08ffb968a24759bb878265ec3951187ac0d59
ikngtty <ikngtty@gmail.com>
2020-03-29 15:48:52 +0900

    (Go) improve performance for scanning

--- AtCoder/Golang/main.go
+++ AtCoder/Golang/main.go
@@ -1,18 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Scanner struct {
+	bufScanner *bufio.Scanner
+}
+
+func NewScanner() *Scanner {
+	bufSc := bufio.NewScanner(os.Stdin)
+	bufSc.Split(bufio.ScanWords)
+	return &Scanner{bufSc}
+}
+
+func (sc *Scanner) ReadString() string {
+	bufSc := sc.bufScanner
+	bufSc.Scan()
+	return bufSc.Text()
+}
+
+func (sc *Scanner) ReadInt() int {
+	bufSc := sc.bufScanner
+	bufSc.Scan()
+	text := bufSc.Text()
+
+	num, err := strconv.Atoi(text)
+	if err != nil {
+		panic(err)
+	}
+	return num
+}
 
 func main() {
-	var n int
-	fmt.Scanf("%d", &n)
+	sc := NewScanner()
 
-	var x, y, z int
-	fmt.Scanf("%d %d %d", &x, &y, &z)
+	n := sc.ReadInt()
+
+	x := sc.ReadInt()
+	y := sc.ReadInt()
+	z := sc.ReadInt()
 
 	a := make([]int, n)
 	b := make([]int, n)
 	for i := 0; i < n; i++ {
-		fmt.Scanf("%d %d", &a[i], &b[i])
+		a[i] = sc.ReadInt()
+		b[i] = sc.ReadInt()
 	}
 
 	total := x + y + z
