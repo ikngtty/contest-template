b33f0c08274079ecef4c8a28f3dbcf28ac0e0930
ikngtty <ikngtty@gmail.com>
2020-05-04 01:19:08 +0900

    (Go) remove int64; it is same as int at AtCoder

--- AtCoder/Golang/main.go
+++ AtCoder/Golang/main.go
@@ -78,47 +78,6 @@ func Pow(base int, exponent uint) int {
 	return answer
 }
 
-func Abs64(x int64) int64 {
-	if x >= 0 {
-		return x
-	}
-	return -x
-}
-
-func Min64(values ...int64) int64 {
-	if len(values) == 0 {
-		panic("no values")
-	}
-	min := values[0]
-	for _, v := range values {
-		if v < min {
-			min = v
-		}
-	}
-	return min
-}
-
-func Max64(values ...int64) int64 {
-	if len(values) == 0 {
-		panic("no values")
-	}
-	max := values[0]
-	for _, v := range values {
-		if v > max {
-			max = v
-		}
-	}
-	return max
-}
-
-func Pow64(base int64, exponent uint) int64 {
-	answer := int64(1)
-	for i := uint(0); i < exponent; i++ {
-		answer *= base
-	}
-	return answer
-}
-
 // *   io
 
 type Scanner struct {
@@ -144,18 +103,6 @@ func (sc *Scanner) ReadInt() int {
 	text := bufSc.Text()
 
 	num, err := strconv.Atoi(text)
-	if err != nil {
-		panic(err)
-	}
-	return num
-}
-
-func (sc *Scanner) ReadInt64() int64 {
-	bufSc := sc.bufScanner
-	bufSc.Scan()
-	text := bufSc.Text()
-
-	num, err := strconv.ParseInt(text, 10, 64)
 	if err != nil {
 		panic(err)
 	}
