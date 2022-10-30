7bd38b6dbcfe265198a283a6d95bdc5cc7128331
ikngtty <ikngtty@gmail.com>
2022-10-30 18:46:47 +0900

    (Go) add `arrayutil`

--- AOJ/Golang/main.go
+++ AOJ/Golang/main.go
@@ -110,6 +110,70 @@ func EucRem(divident, dividor int) int {
 	return rem
 }
 
+// package arrayutil
+
+// MakeBoolArray returns a slice of the bool array.
+func MakeBoolArray(length int, initVal bool) []bool {
+	a := make([]bool, length)
+
+	if initVal {
+		for i := 0; i < length; i++ {
+			a[i] = initVal
+		}
+	}
+
+	return a
+}
+
+// Make2DBoolArray returns a slice of the two-dimensional bool array.
+func Make2DBoolArray(xLen, yLen int, initVal bool) [][]bool {
+	a := make([][]bool, xLen)
+	for x := 0; x < xLen; x++ {
+		a[x] = make([]bool, yLen)
+	}
+
+	if initVal {
+		for x := 0; x < xLen; x++ {
+			for y := 0; y < yLen; y++ {
+				a[x][y] = initVal
+			}
+		}
+	}
+
+	return a
+}
+
+// MakeIntArray returns a slice of the int array.
+func MakeIntArray(length int, initVal int) []int {
+	a := make([]int, length)
+
+	if initVal != 0 {
+		for i := 0; i < length; i++ {
+			a[i] = initVal
+		}
+	}
+
+	return a
+}
+
+// Make2DIntArray returns a slice of the two-dimensional int array.
+func Make2DIntArray(xLen, yLen int, initVal int) [][]int {
+	a := make([][]int, xLen)
+	for x := 0; x < xLen; x++ {
+		a[x] = make([]int, yLen)
+	}
+
+	if initVal != 0 {
+		for x := 0; x < xLen; x++ {
+			for y := 0; y < yLen; y++ {
+				a[x][y] = initVal
+			}
+		}
+	}
+
+	return a
+}
+
 // package sortutil
 
 // ReverseInts sorts a reversely.
